91cb5a405681b549a9635f98300d988eb526f0f3
PohribniakDaryna <dpogribnyak@ukr.net>
2023-04-25 11:44:28 +0300

    added HW 4 (created class Extension with method Doublework and  class PublishingHouse with method Create)

---
+++ HW1ClassesHierarchy/Extension.cs
@@ -0,0 +1,13 @@
+﻿namespace HW1ClassesHierarchy
+{
+    public static class Extension
+    {
+        public static void DoubleWork<T>(this T value, Source source, string review, int rating) where T : IAction, IReviewSources
+        {
+            value.OpenSource();
+            value.CloseSource();
+            value.AddSourceReview(review, source);
+            value.AddSourseRating(rating, source);
+        }
+    }
+}
---
+++ HW1ClassesHierarchy/PublishingHouse.cs
@@ -0,0 +1,10 @@
+﻿namespace HW1ClassesHierarchy
+{
+    public class PublishingHouse<T> where T : class, new()
+    {
+        public T Create()
+        {
+            return new T();
+        }
+    }
+}
